3006da69193f6765f3fbb41806352b19fb66e6cc
AdamManselll <adammansellhmb@gmail.com>
2020-08-14 09:50:40 -0700

    Add js to show java, react now. Hide & show will also be added for divs

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
 $(document).ready(function(){
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    
+
     const question1Input = $("input:radio[name=question1]:checked").val();
     const question2Input = $("input:radio[name=question2]:checked").val();
     const question3Input = $("input:radio[name=question3]:checked").val();
 
     if (question1Input === "cats" && question2Input === "inside" && question3Input === "left") {
+      $("#java").hide();
+      $("#react").hide();
       $("#python").show();
-    } else if (question1Input === "biden" && question2Input === "blue") {
-      $("#liberal").show();
-    } else if (question1Input === "trump" && question2Input === "red") {
-      $("#conservative").show();
-    } else {
-      $("#moderate").show();
+    } else if (question1Input === "cats" && question2Input === "outside" && question3Input === "left") {
+      $("#python").hide();
+      $("#react").hide();
+      $("#java").show();
+    } else if (question1Input === "chins" && question2Input === "inside" && question3Input === "middle") {
+      $("#java").hide();
+      $("#python").hide();
+      $("#react").show();
     }
   });
 });
