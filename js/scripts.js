c297194c179413df136b1b674c45505242ac81c8
AdamManselll <adammansellhmb@gmail.com>
2020-08-14 10:32:40 -0700

    Organize code, indentation and comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
   $("#formOne").submit(function(event) {
     event.preventDefault();
-
+    // -------------- Variables ---------------
     const question1Input = $("input:radio[name=question1]:checked").val();
     const question2Input = $("input:radio[name=question2]:checked").val();
     const question3Input = $("input:radio[name=question3]:checked").val();
     const question4Input = $("input:radio[name=question4]:checked").val();
     const question5Input = $("input:radio[name=question5]:checked").val();
-
+    // -------- Conditional Statements ---------
     if (question1Input === "cats" && question2Input === "inside" && question3Input === "left" && question4Input === "push" && question5Input === "top") {
       $("#java").hide();
       $("#react").hide();
