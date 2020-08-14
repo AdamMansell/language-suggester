28aa900c6d005a0a191078f367f2c26b52bcc3f2
AdamManselll <adammansellhmb@gmail.com>
2020-08-14 10:38:37 -0700

    Add & Show question 6. Will also add js for question to affect result.

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,17 @@ $(document).ready(function(){
     const question3Input = $("input:radio[name=question3]:checked").val();
     const question4Input = $("input:radio[name=question4]:checked").val();
     const question5Input = $("input:radio[name=question5]:checked").val();
+    const question6Input = $("input:radio[name=question6]:checked").val();
     // -------- Conditional Statements ---------
-    if (question1Input === "cats" && question2Input === "inside" && question3Input === "left" && question4Input === "push" && question5Input === "top") {
+    if (question1Input === "cats" && question2Input === "inside" && question3Input === "left" && question4Input === "push" && question5Input === "top" && question6Input === "rice") {
       $("#java").hide();
       $("#react").hide();
       $("#python").show();
-    } else if (question1Input === "cats" && question2Input === "outside" && question3Input === "left" && question4Input === "pull" && question5Input === "bottom") {
+    } else if (question1Input === "chins" && question2Input === "outside" && question3Input === "right" && question4Input === "pull" && question5Input === "bottom" && question6Input === "backpack") {
       $("#python").hide();
       $("#react").hide();
       $("#java").show();
-    } else if (question1Input === "chins" && question2Input === "inside" && question3Input === "middle" && question4Input === "push" && question5Input === "side") {
+    } else if (question1Input === "chins" && question2Input === "inside" && question3Input === "middle" && question4Input === "push" && question5Input === "side" && question6Input === "potatoe") {
       $("#java").hide();
       $("#python").hide();
       $("#react").show();
