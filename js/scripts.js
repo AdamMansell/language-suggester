710c6f2cc10e299403445479a891dfa6f1f93aff
AdamManselll <adammansellhmb@gmail.com>
2020-08-14 09:18:59 -0700

    Add JS for python div to show

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,18 @@ $(document).ready(function(){
   $("#formOne").submit(function(event) {
     event.preventDefault();
     
+    const question1Input = $("input:radio[name=question1]:checked").val();
+    const question2Input = $("input:radio[name=question2]:checked").val();
+    const question3Input = $("input:radio[name=question3]:checked").val();
+
+    if (question1Input === "cats" && question2Input === "inside" && question3Input === "left") {
+      $("#python").show();
+    } else if (question1Input === "biden" && question2Input === "blue") {
+      $("#liberal").show();
+    } else if (question1Input === "trump" && question2Input === "red") {
+      $("#conservative").show();
+    } else {
+      $("#moderate").show();
+    }
   });
 });
