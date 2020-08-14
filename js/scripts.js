614c278a27cff5e4a4a131c377dc826a36ca9b7f
AdamManselll <adammansellhmb@gmail.com>
2020-08-14 10:16:35 -0700

    Add & Show question 5. Will also add js for question to affect result.

--- js/scripts.js
+++ js/scripts.js
@@ -6,22 +6,24 @@ $(document).ready(function(){
     const question2Input = $("input:radio[name=question2]:checked").val();
     const question3Input = $("input:radio[name=question3]:checked").val();
     const question4Input = $("input:radio[name=question4]:checked").val();
+    const question5Input = $("input:radio[name=question5]:checked").val();
 
-    if (question1Input === "cats" && question2Input === "inside" && question3Input === "left" && question4Input === "push") {
+    if (question1Input === "cats" && question2Input === "inside" && question3Input === "left" && question4Input === "push" && question5Input === "top") {
       $("#java").hide();
       $("#react").hide();
       $("#python").show();
-    } else if (question1Input === "cats" && question2Input === "outside" && question3Input === "left" && question4Input === "pull") {
+    } else if (question1Input === "cats" && question2Input === "outside" && question3Input === "left" && question4Input === "pull" && question5Input === "bottom") {
       $("#python").hide();
       $("#react").hide();
       $("#java").show();
-    } else if (question1Input === "chins" && question2Input === "inside" && question3Input === "middle" && question4Input === "push") {
+    } else if (question1Input === "chins" && question2Input === "inside" && question3Input === "middle" && question4Input === "push" && question5Input === "side") {
       $("#java").hide();
       $("#python").hide();
       $("#react").show();
     } else {
       $("#java").hide();
       $("#python").hide();
+      $("#react").hide();
       $("#ruby").show();
     }
   });
