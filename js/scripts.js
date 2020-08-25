c84f70f74478d1829ffd360520c8d9ffedf3b670
AdamManselll <adammansellhmb@gmail.com>
2020-08-24 17:58:09 -0700

    Add image to screen, still need to configure example code to work with personal goal for project.

--- js/scripts.js
+++ js/scripts.js
@@ -42,3 +42,18 @@ $(document).ready(function(){
     document.getElementById("formOne").reset();
   });
 });
+
+
+var velocity = 0.5;
+
+function update(){ 
+    var pos = $(window).scrollTop(); 
+    $('.container').each(function() { 
+        var $element = $(this);
+        // subtract some from the height b/c of the padding
+        var height = $element.height();
+        $(this).css('backgroundPosition', '100% ' + Math.round((height - pos) * velocity) + 'px'); 
+    }); 
+};
+
+$(window).bind('scroll', update);
